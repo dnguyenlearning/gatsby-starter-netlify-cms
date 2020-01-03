e6c8795b678c43ef88448f2e26973d1e5788a800
Duong Nguyen <dng@localizedirect.com>
2020-01-04 01:01:59 +0700

    1

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,10 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+        enableIdentityWidget: true,
+        publicPath: `admin`,
+        htmlTitle: `Cong Tech Post Manager`,
+        includeRobots: true,
       },
     },
     {
